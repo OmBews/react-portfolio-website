b5b9624fd10b1b3cd434a37ab94ce6b5857b421d
Tien Le <LeDaTi01@risk.regn.net>
2022-03-26 21:15:21 +0700

    Add product and product list components

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+        id: 1,
+        img: "https://colorlib.com/cdn-cgi/image/quality=80,format=auto,onerror=redirect,metadata=none/wp/wp-content/uploads/sites/2/academia-free-template.jpg",
+        link: "http://lama.dev",
+    },
+    {
+        id: 2,
+        img: "https://colorlib.com/cdn-cgi/image/quality=80,format=auto,onerror=redirect,metadata=none/wp/wp-content/uploads/sites/2/knuford-free-template.jpg",
+        link: "http://lama.dev",
+    },
+    {
+        id: 3,
+        img: "https://bootstrapmade.com/content/templatefiles/Ninestars/Ninestars-bootstrap-website-template-md.png",
+        link: "http://lama.dev",
+    },
+    {
+        id: 4,
+        img: "https://colorlib.com/cdn-cgi/image/quality=80,format=auto,onerror=redirect,metadata=none/wp/wp-content/uploads/sites/2/saita-colorlib-template.jpg",
+        link: "http://lama.dev",
+    },
+    {
+        id: 5,
+        img: "https://themefisher.com/wp-content/uploads/edd/2019/04/bexer-bootstrap-business-template.jpg",
+        link: "http://lama.dev",
+    },
+    {
+        id: 6,
+        img: "https://assets-global.website-files.com/5e593fb060cf877cf875dd1f/60b6b54cca1a1af1b2a9acea_gallery01.jpeg",
+        link: "http://lama.dev",
+    },
+];
