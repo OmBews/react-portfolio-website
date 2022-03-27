bdd8e8657abe9244953540047342f8d279392e62
Tiến Lê <tien.le@zamo.io>
2022-03-27 20:32:30 +0700

    Support responsive

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,16 @@
-import React, { useRef } from 'react'
+import React, { useContext, useRef } from 'react'
 import './contact.css'
 import Phone from '../../img/phone.png'
 import Email from '../../img/email.png'
 import Address from '../../img/address.png'
 import emailjs from '@emailjs/browser';
+import { ThemeContext } from '../../context'
 
 function Contact() {
     const formRef = useRef();
+    const theme = useContext(ThemeContext);
+    const darkMode = theme.state.darkMode;
+
     const handleSubmit = (e) => {
         e.preventDefault();
         emailjs.sendForm('service_qg3wzxh', 'template_0nbs99f', formRef.current, 'DtTdQJq2LAKh_dTeS')
@@ -25,7 +29,7 @@ function Contact() {
         </div>
         <div className="c-wrapper">
                 <div className="c-left">
-                    <h1 className="c-title">Let's duscuss your project</h1>
+                    <h1 className="c-title">Let's discuss your project</h1>
                     <div className="c-info">
                         <div className="c-info-item">
                             <img src={Phone} alt="" className="c-icon" /> +84 834 020 796
@@ -41,13 +45,13 @@ function Contact() {
                 <div className="c-right">
                     <p className="c-desc">
                         <b>What’s your story?</b> Get in touch. Always available for
-                        freelancing if the right project comes along. me.
+                        freelancing if the suitable project comes.
                     </p>
                     <form ref={formRef} onSubmit={handleSubmit}>
-                        <input type="text" placeholder='Name' name='user_name' />
-                        <input type="text" placeholder='Subject' name='user_subject' />
-                        <input type="text" placeholder='Email' name='user_email' />
-                        <textarea cols="30" rows="5" placeholder='Message' name='message'></textarea>
+                        <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder='Name' name='user_name' />
+                        <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder='Subject' name='user_subject' />
+                        <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder='Email' name='user_email' />
+                        <textarea style={{backgroundColor: darkMode && "#333"}} cols="30" rows="5" placeholder='Message' name='message'></textarea>
                         <button>Submit</button>
                     </form>
                 </div>
