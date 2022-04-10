9d03dae1161c1ace2beb3ce507ed067b2dcd87da
Tiến Lê <tien.le@zamo.io>
2022-04-10 16:39:51 +0700

    Update project link and image

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -5,9 +5,9 @@ function Product({img, link}) {
   return (
     <div className='p'>
       <div className="p-browser">
-        <div className="p-circle"></div>
-        <div className="p-circle"></div>
-        <div className="p-circle"></div>
+        <div className="p-circle red"></div>
+        <div className="p-circle yellow"></div>
+        <div className="p-circle green"></div>
       </div>
       <a href={link} target="_blank" rel="noreferrer">
         <img src={img} alt="" className="p-img" />
--- src/data.js
+++ src/data.js
@@ -1,13 +1,13 @@
 export const products = [
     {
         id: 1,
-        img: "https://colorlib.com/cdn-cgi/image/quality=80,format=auto,onerror=redirect,metadata=none/wp/wp-content/uploads/sites/2/academia-free-template.jpg",
-        link: "http://lama.dev",
+        img: "https://i.ibb.co/ChBjFdG/Screen-Shot-2022-04-10-at-16-42-42.png",
+        link: "https://waveportal-starter-project.dactien.repl.co/",
     },
     {
         id: 2,
-        img: "https://colorlib.com/cdn-cgi/image/quality=80,format=auto,onerror=redirect,metadata=none/wp/wp-content/uploads/sites/2/knuford-free-template.jpg",
-        link: "http://lama.dev",
+        img: "https://i.ibb.co/FB3s3RY/Screen-Shot-2022-04-11-at-14-50-12.png",
+        link: "https://nft-starter-project.dactien.repl.co/",
     },
     {
         id: 3,
